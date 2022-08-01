cd95fbe9ba2c80771971ffa16cb38963a0376483
miniloda <jlusaraga@up.edu.ph>
2022-08-01 20:30:14 +0800

    Fixed /year bug

--- src/script/script.js
+++ src/script/script.js
@@ -5,19 +5,15 @@ changeChoice.addEventListener("click",()=>{
     console.log(document.getElementsByClassName("per"));
     if(choice === "annually"){
         choice = "monthly";
-        document.getElementById("basic").innerText = "$5"
+        
         document.getElementsByClassName("per")[0].innerText = "/month";
-        document.getElementsByClassName("per")[1].innerText = "/month";
-        document.getElementsByClassName("per")[2].innerText = "/month";
-        document.getElementById("professional").innerText = "$10";
+        document.getElementById("professional").innerHTML = "$10<span class='per'>/month</span>";
+        document.getElementById("basic").innerHTML = "$5<span class='per'>/month</span>";
     }
     else{
         choice = "annually";
-        document.getElementById("basic").innerText = "$55";
         document.getElementsByClassName("per")[0].innerText = "/year";
-        document.getElementsByClassName("per")[1].innerText = "/year";
-        document.getElementsByClassName("per")[2].innerText = "/month";
-        document.getElementById("professional").innerText = "$100";
+        document.getElementById("professional").innerHTML = "$100<span class='per'>/year</span>";
+        document.getElementById("basic").innerHTML = "$55<span class='per'>/year</span>";
     }
-    return true;
 });
